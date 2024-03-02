a05055466e2a33509adca8ee639c9134ed11b5b1
Gabriel Gavin <ggavin@outlook.com>
2024-03-02 10:22:26 -0500

    rename options manager to cauldron manager

--- Assets/Scripts/CauldronManager.cs
+++ Assets/Scripts/CauldronManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionsManager : MonoBehaviour
+public class CauldronManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
