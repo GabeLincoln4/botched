9d806d6989a8dfe065d88741b75827166c83ad4a
Leslie Benitez <ggavin@outlook.com>
2024-07-29 12:21:52 -0400

    create a list variable scriptable object

---
+++ Assets/Scripts/ListVariableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListVariableObject : ScriptableObject
+{
+    public List<Transform> _gameObjects;
+}
