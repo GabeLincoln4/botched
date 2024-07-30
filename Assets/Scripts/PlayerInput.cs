8298072f9fbbe63b6c3773476455601b5fda1e92
Leslie Benitez <ggavin@outlook.com>
2024-07-29 20:56:32 -0400

    add additional inputs to play input class

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,11 +7,35 @@ public class PlayerInput : MonoBehaviour
     [SerializeField]
     private GameEvent _onQKeyPressed;
 
+    [SerializeField]
+    private GameEvent _onWKeyPressed;
+
+    [SerializeField]
+    private GameEvent _onAKeyPressed;
+
+    [SerializeField]
+    private GameEvent _onSKeyPressed;
+
     private void Update ()
     {
         if (Input.GetKeyDown("q"))
         {
             _onQKeyPressed.Raise();
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            _onWKeyPressed.Raise();
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            _onAKeyPressed.Raise();
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            _onSKeyPressed.Raise();
         }
     }
 }
