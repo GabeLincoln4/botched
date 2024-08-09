762324567363d7391c59e116cb388a79b891018e
Leslie Benitez <ggavin@outlook.com>
2024-08-08 22:51:33 -0400

    start processing of debugging in order to cut down on the number of game event listener responses used to activate the grimoire and keep it in a loop

--- Assets/Scripts/IngredientFunctionLibrary.cs
+++ Assets/Scripts/IngredientFunctionLibrary.cs
@@ -11,6 +11,12 @@ public class IngredientFunctionLibrary : MonoBehaviour
     [SerializeField]
     private ListOfBooleanVariableObject _listOfBooleanVariableObject;
 
+    [SerializeField]
+    private GameEvent _onTrueVariable;
+
+    [SerializeField]
+    private GameEvent _onFalseVariable;
+
     public void AddGameObjectToList (Transform gameObject)
     {
         _ingredients._gameObjects.Add(gameObject);
@@ -19,6 +25,23 @@ public class IngredientFunctionLibrary : MonoBehaviour
     public void AddBooleanVariableToListObject ()
     {
         _listOfBooleanVariableObject._booleanValues.Add(false);
+    }
+
+    public void CheckForBooleanState (BooleanVariableObject booleanVariableObject)
+    {
+        if (booleanVariableObject._booleanValue == true)
+        {
+            _onTrueVariable.Raise();
+        }
+        else 
+        {
+            _onFalseVariable.Raise();
+        }
+    }
+
+    public void DebugValue ()
+    {
+        Debug.Log("value is true");
     }
 
     private void OnDisable ()
