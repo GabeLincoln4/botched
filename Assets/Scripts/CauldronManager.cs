7cfaca1e072dc5755421d126379c015d004b6d31
Gabriel Gavin <ggavin@outlook.com>
2024-09-28 20:17:47 -0400

    delete unused classes and game objects

--- Assets/Scripts/CauldronManager.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class CauldronManager : MonoBehaviour {
-
-    private static bool resetInputActivated = false;
-    private static bool submitInputActivated = false;
-}
